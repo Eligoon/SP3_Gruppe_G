e0ec268772a4d479129c20bb4ca81b336c594cb2
Eligoon <derpepind117@live.dk>
2025-11-07 14:10:29 +0100

    Created abstract Media class

---
+++ src/Media.java
@@ -0,0 +1,15 @@
+public abstract class Media
+{
+    protected String name;
+    protected int releaseYear;
+    protected double rating;
+    protected String category;
+
+    public Media(String name, int releaseYear, double rating, String category)
+    {
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+        this.category = category;
+    }
+}
