8fcfe7e4c4a734f2c6d8bdf5debcaab02c32a2b1
Eligoon <derpepind117@live.dk>
2025-11-10 09:21:02 +0100

    created MainMenu method for controlling where you want to go and do in the streamingservice

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -1,3 +1,5 @@
+import utility.TextUI;
+
 import java.util.*;
 
 public class StreamingService {
@@ -6,11 +8,56 @@ public class StreamingService {
     private List<Series> series;
     private ArrayList<User> users = new ArrayList<>();
 
+    TextUI ui = new TextUI();
+
     public StreamingService() {
         this.scanner = new Scanner(System.in);
         this.movies = new ArrayList<>();
         this.series = new ArrayList<>();
     }
+
+    private void mainMenu()
+    {
+// Define menu options
+        ArrayList<String> menuOptions = new ArrayList<>();
+        menuOptions.add("Search for media by name");
+        menuOptions.add("Search for media by category");
+        menuOptions.add("Get list of your saved movies");
+        menuOptions.add("Get list of movies you have already seen");
+        menuOptions.add("Exit streaming service");
+
+        while (true)
+        {
+            // Prompt user for input using TextUI
+            int choice = ui.promptMenu("Main Menu", menuOptions);
+
+            // Handle the menu choice
+            switch (choice)
+            {
+                case 1:
+                   searchByName();
+                    break;
+                case 2:
+                    searchByCategory();
+                    break;
+                case 3:
+                    getListOfSaved();
+                    break;
+                case 4:
+                    getListOfWatched();
+                    break;
+                case 5:
+                    // Exit the program safely
+                    System.out.println("Exiting streaming service.");
+
+                    System.exit(0);
+                default:
+                    // If the input doesn't match a valid option
+                    System.out.println("Invalid choice, try again.");
+            }
+        }
+    }
+
     // Getters and setters
     public Scanner getScanner() {
         return scanner;
