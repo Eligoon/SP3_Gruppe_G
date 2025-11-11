6183589187d9d0ccd041cc77fdff124a2fefce5d
Eligoon <derpepind117@live.dk>
2025-11-11 14:38:55 +0100

    u correcte dto current users

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -152,14 +152,14 @@ public class StreamingService {
 
     public void getListOfSaved() {
         ArrayList<Media> list = new ArrayList<>();
-        list = u.getWantsToSee(); // u er den bruger som er logget ind
+        list = currentUser.getWantsToSee(); // u er den bruger som er logget ind
         int choice = ui.promptMenu("Select media", list);
         list.get(choice).playMedia();
     }
 
     public void getListOfWatched() {
         ArrayList<Media> list = new ArrayList<>();
-        list = u.getSeenMedia(); // u er den bruger som er logget ind
+        list = currentUser.getSeenMedie(); // u er den bruger som er logget ind
         int choice = ui.promptMenu("Select media", list);
         list.get(choice).playMedia();
     }
