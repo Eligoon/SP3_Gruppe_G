9fc4377c9932bae060624d88e506de229bee0a25
Eligoon <derpepind117@live.dk>
2025-11-12 13:11:48 +0100

    updated series to handle start and end years

--- src/Series.java
+++ src/Series.java
@@ -1,10 +1,14 @@
 import utility.TextUI;
+import java.util.List;
 
-public class Series extends Media implements  Playable{
+public class Series extends Media implements Playable {
     int season;
     int episode;
     int endYear;
+    List<String> seasons;   // for summary style
+    String yearRange;       // for summary style
 
+    // Detailed constructor
     Series(String name, int releaseYear, double rating, String category, int season, int episode, int endYear) {
         super(name, releaseYear, rating, category);
         this.season = season;
@@ -12,31 +16,31 @@ public class Series extends Media implements  Playable{
         this.endYear = endYear;
     }
 
+    // Summary constructor for CSV loader
+    Series(String name, String yearRange, double rating, String category, List<String> seasons) {
+        super(name, 0, rating, category); // 0 placeholder for releaseYear
+        this.yearRange = yearRange;
+        this.seasons = seasons;
+    }
+
     public void playMedia(User u) {
         TextUI ui = new TextUI();
         ui.displayMsg("1. Play " + this);
         ui.displayMsg("2. Save " + this + " to your list");
         boolean continueLoop = true;
-        while(continueLoop) {
+        while (continueLoop) {
             int choice = ui.promptNumeric("Type 1 or 2");
-            if(choice == 1) {
+            if (choice == 1) {
                 ui.displayMsg("Now playing: " + this);
-                // tilføjer medie til brugerens seenMedia liste
                 u.getSeenMedia().add(this);
                 continueLoop = false;
-            } else if(choice == 2) {
-                ui.displayMsg(this + "has been added to your list");
-                // tilføjer medie til brugerens wantsToSee liste
+            } else if (choice == 2) {
+                ui.displayMsg(this + " has been added to your list");
                 u.getWantsToSee().add(this);
                 continueLoop = false;
             } else {
                 ui.displayMsg("Invalid number");
             }
-            //skift "this" til "this.name" hvis vi ændrer toString
         }
-    }
-
-    public String toString() {
-        return name;
     }
 }
